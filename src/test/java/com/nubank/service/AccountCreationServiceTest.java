f8d36ca4bb5af64d3587b777cb82577763f6f00b
Iván Javier Romero <ayoros@gmail.com>
2020-02-02 08:00:54 -0500

    Separate each approved transactions per account

--- src/test/java/com/nubank/service/AccountCreationServiceTest.java
+++ src/test/java/com/nubank/service/AccountCreationServiceTest.java
@@ -2,6 +2,7 @@ package com.nubank.service;
 
 import com.nubank.model.Account;
 import com.nubank.model.Bank;
+import com.nubank.model.Transaction;
 import com.nubank.model.Violations;
 import io.vavr.collection.HashMap;
 import io.vavr.collection.List;
@@ -28,7 +29,7 @@ class AccountCreationServiceTest {
     @Test
     void testNoViolationExistingOtherAccounts() {
         //give
-        Bank bank = new Bank(getCurrentAccounts(), List.empty());
+        Bank bank = new Bank(getCurrentAccounts(), getEmptyTransactionsForCurrentAccounts());
         Account newAccount = new Account("2", true, 300);
 
         //when
@@ -41,7 +42,7 @@ class AccountCreationServiceTest {
     @Test
     void testAccountAlreadyExist() {
         //given
-        Bank bank = new Bank(getCurrentAccounts(), List.empty());
+        Bank bank = new Bank(getCurrentAccounts(), getEmptyTransactionsForCurrentAccounts());
         Account account = new Account("1", true, 200);
 
         //when
@@ -53,5 +54,11 @@ class AccountCreationServiceTest {
 
     private Map<String, Account> getCurrentAccounts() {
         return HashMap.ofEntries(Map.entry("1", new Account("1", true, 100)));
+    }
+
+    private Map<String, List<Transaction>> getEmptyTransactionsForCurrentAccounts() {
+        return HashMap.ofEntries(
+                Map.entry("1", List.empty())
+        );
     }
 }
